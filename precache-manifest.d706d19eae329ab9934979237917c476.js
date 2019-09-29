9c01e4f9c0ade153f458765fab4f570e39af9e6e
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-29 19:15:43 +0300

    Updates

---
+++ precache-manifest.d706d19eae329ab9934979237917c476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8a0fc0be63342adb41ff25a3c34470",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ca5254b4a437dbc7108f",
+    "url": "/react_tabs-with-router/static/css/main.31ce9786.chunk.css"
+  },
+  {
+    "revision": "a5046cfd21620cae9bae",
+    "url": "/react_tabs-with-router/static/js/2.10e3d643.chunk.js"
+  },
+  {
+    "revision": "ca5254b4a437dbc7108f",
+    "url": "/react_tabs-with-router/static/js/main.9adb7514.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
